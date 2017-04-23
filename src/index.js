17ac59e639f051359534aff4e24b582fc4303c20
dylanjshaw <dylanjshaw@sandiego.edu>
2017-04-23 09:41:30 -0700

    commit all

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail'
 const API_KEY = 'AIzaSyBWPrqU7RGkDaS4G-EpCGJosqTmrFdQ7lk';
 
 
@@ -30,7 +31,8 @@ class App extends React.Component {
     return (
       <div>
         <SearchBar />
-        <VideoList videos={this.state.videos} /> 
+        <VideoDetail video={this.state.videos[0]} />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
